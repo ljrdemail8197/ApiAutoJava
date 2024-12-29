a9464194208adf1e869ded6bbc631851110a6c7e
ljrdemail8198 <ljrddemail@gmail.com>
2024-12-29 10:46:56 +0800

    呃呃呃

---
+++ Chapter5/src/main/java/com/course/code/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package com.course.code;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+    @Test
+    public void testCase1(){
+        System.out.println("test case1");
+
+    }
+}
